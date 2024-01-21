fb98bf863b4ea90af1673c36f73cc0f43b8b79cb
kennby <153956421+kennby@users.noreply.github.com>
2024-01-21 04:22:59 -0500

    Update QUEEN-HENTAI-Reply.js

--- plugins/QUEEN-HENTAI-Reply.js
+++ plugins/QUEEN-HENTAI-Reply.js
@@ -5,16 +5,16 @@ let handler = m => m
 handler.all = async function (m) {
 	
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
-	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://i.imgur.com/bFEC7lC.jpeg')
+	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/35390789044f4122413b3.jpg')
 	
 	//reply link wa
-   global.rpl = { contextInfo: { externalAdReply: { mediaUrl: dygp, mediaType: 'VIDEO', description: 'support group', title: packname, body: 'support group', thumbnailUrl: pp, sourceUrl: dygp }}} 
+   global.rpl = { contextInfo: { externalAdReply: { mediaUrl: dygp, mediaType: 'ᴏɴʏx', description: 'ᴏɴʏx', title: packname, body: 'ᴏɴʏx', thumbnailUrl: pp, sourceUrl: dygp }}} 
 	
 	//reply link PayPal
-    global.rpyp = { contextInfo: { externalAdReply: { mediaUrl: dygp, mediaType: 'VIDEO', description: 'Donate', title: 'YOUTUBE', body: 'Keep bot alive', thumbnailUrl: pp, sourceUrl: fgyt }}}
+    global.rpyp = { contextInfo: { externalAdReply: { mediaUrl: dygp, mediaType: 'ᴏɴʏx', description: 'ᴏɴʏx', title: 'ʏᴏᴜᴛᴜʙᴇ', body: 'ᴏɴʏx', thumbnailUrl: pp, sourceUrl: fgyt }}}
 	
 	//reply link yt
-    global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: fgyt, mediaType: 'VIDEO', description: 'Suscribete : ' + fgyt, title: 'YouTube', body: 'learn to create your own bots', thumbnailUrl: pp, sourceUrl: fgyt }}}
+    global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: fgyt, mediaType: 'ᴏɴʏx', description: 'ᴏɴʏx : ' + fgyt, title: 'ʏᴏᴜᴛᴜʙᴇ', body: 'ʙᴏᴛs', thumbnailUrl: pp, sourceUrl: fgyt }}}
 
 } 
 export default handler
