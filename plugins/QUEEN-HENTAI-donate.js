2b124966bba17009658f0cfa4aba014e010f91a2
kennby <153956421+kennby@users.noreply.github.com>
2024-01-21 04:24:29 -0500

    Update QUEEN-HENTAI-donate.js

--- plugins/QUEEN-HENTAI-donate.js
+++ plugins/QUEEN-HENTAI-donate.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
 🇱🇰 *DONATE* 🇱🇰
-If you get more info of 💝 Queen Hentai 💝 Please Contact 94740804536 💰`
-let img = 'https://telegra.ph/file/7d84f663bc6e7fdac5c66.jpg'
+If you get more info of Queen Hentai Please Contact 51902247905 💰`
+let img = 'https://telegra.ph/file/35390789044f4122413b3.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 
